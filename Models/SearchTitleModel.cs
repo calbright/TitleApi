98721deefdc675d3c265ce0b34975ae1f2082852
Chris <chris.albright@bluesombrero.com>
2021-01-17 20:52:52 -0500

    Revert "update readme"
    This reverts commit e264bb203a0bd2d9773fd325482c0e08bbb4c512.

---
+++ Models/SearchTitleModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TitleApi.Models
+{
+    public class SearchTitleModel
+    {
+        public string TitleName { get; set; }
+        public int TitleId { get; set; }
+    }
+}
